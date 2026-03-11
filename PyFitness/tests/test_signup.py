e6a4b55c3fd65e006f25bd9a622bc3ef60973a01
georgevans <lgjt5248@leeds.ac.uk>
2026-03-11 12:58:21 +0000

    Tests for the login route

--- PyFitness/tests/test_signup.py
+++ PyFitness/tests/test_signup.py
@@ -30,3 +30,19 @@ def test_signup_page_contains_email_input():
 def test_signup_page_contains_login_link():
     response = client.get("/signup")
     assert "/login" in response.text
+
+def test_login_page_contains_form():
+    response = client.get("/login")
+    assert "form" in response.text
+
+def test_login_page_contains_username_input():
+    response = client.get("/login")
+    assert "username" in response.text
+
+def test_login_page_contains_password_input():
+    response = client.get("/login")
+    assert "password" in response.text
+
+def test_login_page_contains_signup_link():
+    response = client.get("/login")
+    assert "/signup" in response.text
