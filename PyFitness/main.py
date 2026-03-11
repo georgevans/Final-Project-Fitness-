6253d5aeba819aa26b0b0609a1ce7bb2d03fec62
georgevans <lgjt5248@leeds.ac.uk>
2026-03-11 12:58:07 +0000

    Fixed routing for main page

--- PyFitness/main.py
+++ PyFitness/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from routers import home, signup
 
@@ -9,4 +10,17 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(home.router)
 app.include_router(signup.router)
 
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/signup")
+
 # to run: uvicorn main:app --reload 
+
+"""
+git checkout main
+git pull
+git checkout feature/your-new-feature
+git rebase main
+
+Run this when merge accepted
+"""
