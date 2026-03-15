0bab460bddb8e8f9e78dec5d3c972522c4b192f7
Matthew Ebanks <tngs3474@leeds.ac.uk>
2026-03-15 21:08:07 +0000

    React implementation

---
+++ fitness-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Login from "./Login";
+
+function App(){
+    return(
+        <div>
+            <h1>Fitness App</h1>
+            <p>Welcome to the Fitness App! Track your workouts, monitor your progress, and achieve your fitness goals.</p>
+            <Login />
+        </div>
+    )
+}
+export default App;
---
+++ fitness-app/src/Login.tsx
@@ -0,0 +1,11 @@
+
+function Login(){
+    return(
+        <div>
+            <h1>Login</h1>
+            <p>Login to your account to access your fitness dashboard and track your progress.</p>
+        </div>
+    )
+}
+
+export default Login;
